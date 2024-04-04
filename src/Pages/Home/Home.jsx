da11a2c11c8341d1971ad2ffb64adf150c3ff7e4
Fathi Mohammed <fathymohammed2229@gmail.com>
2024-04-04 13:26:25 +0200

    first commit

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,8 @@
-import { useTranslation } from "react-i18next";
-import styles from './Home.module.scss'
+// import { useTranslation } from "react-i18next";
+// import styles from './Home.module.scss'
 
 export default function Home() {
-  const  [t]  = useTranslation();
+  // const  [t]  = useTranslation();
   return (
     <h1>Home Page</h1>
   )
